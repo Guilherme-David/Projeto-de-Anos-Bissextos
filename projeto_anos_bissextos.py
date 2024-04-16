b308161a3aef20a7eae03f1686edbbe9af420685
Guilherme David <137658009+Guilherme-David@users.noreply.github.com>
2024-04-16 10:33:29 -0300

    Update projeto_anos_bissextos.py

--- projeto_anos_bissextos.py
+++ projeto_anos_bissextos.py
@@ -5,7 +5,10 @@ print(ano_atual_bissexto.MensagemAnoAtual())
 print(ano_atual_bissexto.anoAtualEhBissexto())
 
 print('SEQUÊNCIA DE ANOS BISSEXTOS')
-ano_inicial = int(input('Quer começar por qual ano? '))
-ano_final = int(input('Até que ano? '))
 
-print(anos_bissextos.calcularAnosBissextos([], ano_inicial, ano_final))
+try:
+  ano_inicial = int(input('Quer começar por qual ano? '))
+  ano_final = int(input('Até que ano? '))
+  print(anos_bissextos.calcularAnosBissextos([], ano_inicial, ano_final))
+except ValueError:
+  print('O valor informado foi inválido, \nesperava que fosse digitado um ano')
